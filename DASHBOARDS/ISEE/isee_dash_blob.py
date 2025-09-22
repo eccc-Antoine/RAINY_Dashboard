56d8d90d19554b941ef7b569bad338766941bb3d
Antoine Maranda <antoine.maranda@ec.gc.ca>
2025-09-22 15:51:10 -0400

    first try

--- DASHBOARDS/ISEE/isee_dash_blob.py
+++ DASHBOARDS/ISEE/isee_dash_blob.py
@@ -33,7 +33,7 @@ import io
 
 print('allo')
 
-container_name='isee-test
+container_name='isee-test'
 container_client=UTILS.connect_blob( container_name, env_var_name="AZURE_STORAGE_CONNECTION_STRING", )
 
 def get_env_var(var, env_name):
